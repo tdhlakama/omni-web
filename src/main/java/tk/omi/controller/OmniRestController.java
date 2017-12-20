41ad767f3f7866ae70e7a4261779583a27bc9b73
tdhlakama <tdhlakama@hitrac.co.zw>
2017-12-20 10:44:38 +0200

    get logged in user from principal

--- src/main/java/tk/omi/controller/OmniRestController.java
+++ src/main/java/tk/omi/controller/OmniRestController.java
@@ -3,6 +3,8 @@ package tk.omi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -12,7 +14,6 @@ import tk.omi.model.User;
 import tk.omi.service.AppService;
 
 import javax.servlet.annotation.MultipartConfig;
-import javax.xml.ws.Response;
 import java.io.IOException;
 import java.util.List;
 
@@ -26,6 +27,9 @@ public class OmniRestController {
 
     @PostMapping(value = "/customer/register", produces = "application/json")
     public ResponseEntity saveCustomers(@RequestBody Customer customer) {
+
+        User user = getUser();
+        customer.setUser(user);
         customer = appService.save(customer);
         ServerResponse response = new ServerResponse(customer.getAccountNumber(), "Customer Account Created");
         return new ResponseEntity<>(response, HttpStatus.CREATED);
@@ -37,6 +41,7 @@ public class OmniRestController {
                                                @RequestParam("documentType") String documentType) {
 
         CustomerDocument customerDocument = new CustomerDocument();
+
         List<MultipartFile> files = multipartHttpServletRequest.getFiles("file");
         try {
 
@@ -51,20 +56,39 @@ public class OmniRestController {
                 customerDocument = appService.save(customerDocument);
 
                 ServerResponse response = new ServerResponse(customerDocument.getId(), "Customer Account Created");
-                return ResponseEntity.ok(response);
-
+                return new ResponseEntity<>(response, HttpStatus.CREATED);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         ServerResponse response = new ServerResponse(0L, "Document not saved");
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/user")
-    public ResponseEntity login(@RequestParam("username") String username) {
-        User user = appService.findByUsername(username);
-        return new ResponseEntity<>(user, HttpStatus.FOUND);
+    public ResponseEntity login() {
+
+        User user = getUser();
+        if (user != null)
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        else
+            return new ResponseEntity(null, HttpStatus.NOT_FOUND);
     }
+
+
+    private String getUsername() {
+        User user = getUser();
+        return user != null ? user.getUsername() : "";
+
+    }
+
+    private User getUser() {
+        UserDetails userDetail = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (userDetail != null)
+            return appService.findByUsername(userDetail.getUsername());
+        else
+            return null;
+    }
+
 
 }
